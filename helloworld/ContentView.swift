6239c5df86bb0ee84a2ff5252945bb63a3ff88a9
Stanislav Bondarenko <bondarenko.stanislav.96@gmail.com>
2020-06-01 17:05:05 +0300

    init

---
+++ helloworld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloworld
+//
+//  Created by Stanislav Bondarenko on 01.06.2020.
+//  Copyright © 2020 Stanislav Bondarenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
